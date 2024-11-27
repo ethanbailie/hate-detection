eaf05c82021d486aa257f8367f944e8fe0c695d9
Ethan Bailie <eabailie@uwaterloo.ca>
2024-11-27 00:51:57 -0500

    created sagemaker estimator

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ using finetuned BERT to detect hate speech
 
 execute local training with (adjust epochs as needed):
 .venv/bin/python training.py --data_dir ./data --model_dir ./model_output --epochs 1 --batch_size 4 --learning_rate 2e-5 --num_labels 3
+
+execute sagemaker training by running the sm_estimator.py script 
+sagemaker requirements:
+- set AWS credentials in environment variables
+- set ARN in .env file
+- edit s3 bucket in sm_estimator.py to match where you put the data
---
+++ sm_estimator.py
@@ -0,0 +1,39 @@
+import sagemaker
+from sagemaker.huggingface import HuggingFace
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+## initialize sagemaker session
+sagemaker_session = sagemaker.Session()
+
+## define iam role with sagemaker permissions
+role = os.getenv('ARN')
+
+## define hyperparameters
+hyperparameters = {
+    'epochs': 3,
+    'batch_size': 16,
+    'learning_rate': 2e-5
+}
+
+## define huggingface estimator with output_path set to your s3 bucket
+huggingface_estimator = HuggingFace(
+    entry_point='training.py',                    
+    source_dir='.',           
+    instance_type='ml.p3.2xlarge',                
+    instance_count=1,
+    role=role,
+    transformers_version='4.6',                    
+    pytorch_version='1.7',                         
+    py_version='py36',                             
+    hyperparameters=hyperparameters,
+    output_path='s3://hate-detection'             
+)
+
+## define the s3 path to your training data
+training_data_uri = 's3://hate-detection/data'
+
+## start the training job by specifying the input data location
+huggingface_estimator.fit({'train': training_data_uri})
